d9cd3093e0648c43f26083dfc832e2d48680e3bc
Sunny Jovita <sunnyjovitaaa@gmail.com>
2021-09-20 22:43:19 +0700

    updated_1

--- bobo_bot_sunny.py
+++ bobo_bot_sunny.py
@@ -1,7 +1,20 @@
-discord_token = "ODg5NTE4NjY1MzEyMTEyNjUw.YUia0A.cWUWSVnJY9CF8-XxqXJR07LuyAo"
+# discord_token = "ODg5NTE4NjY1MzEyMTEyNjUw.YUia0A.cWUWSVnJY9CF8-XxqXJR07LuyAo"
 
 import discord
-from discord.ext import commands, tasks
-import os
-# from dotenv, import load_dotenv
-import youtube_dl
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('ODg5NTE4NjY1MzEyMTEyNjUw.YUia0A.cWUWSVnJY9CF8-XxqXJR07LuyAo')
+
